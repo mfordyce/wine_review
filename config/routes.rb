7e02ce0151deee2ec9fb2aa8ac4225692a0900e3
Mark Fordyce <mfordyce97@gmail.com>
2015-12-15 15:31:16 -0800

    Class 02 Homework

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
+
+  get "/wines" => "wines#index", as: 'wines'
+
+  get 'wines/index'
+
+
+
+  get 'wines/show'
+
+
+  get "/wines/:id" => "wines#show", as: 'wine'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Wine.create(name: 'Old Vine Red Lot Number 42', year: 2006,
+  winery: 'Marietta Cellars', varietal: 'California Red Wine',
+  country: 'USA')
+
+Wine.create(name:'Pinot Evil', year: 2005,
+  winery: 'Vin de Pays de l\'isle de Beaute', varietal: 'Pino Noir',
+  country: 'France')
+
+Wine.create(name:'Petit Manseng', year: 2006,
+  winery: 'White Hall Vineyards', varietal: 'Virginia Table Wine',
+  country: 'USA')
+
+Wine.create(name:'Saffire', year: 2003,
+  winery: 'AM Rhine', varietal: 'White Wine Blend',
+  country: 'USA')
+
+Wine.create(name:'Late Harvest Vidal Blanc', year: 2005,
+  winery: 'First Colony Winery', varietal: 'Vidal Blanc',
+  country: 'USA')
+
+Wine.create(name:'De Chiel Reserve Merlot', year: 2006,
+  winery: 'Rockbridge Vineyards', varietal: 'Merlot',
+  country: 'USA')
+
+Wine.create(name:'Ruffino Chianti', year: 2013,
+  winery: 'Casa Vinicola Ruffino', varietal: 'Red Wine',
+  country: 'Italy')
+
+Wine.create(name:'Sangiovese', year: 2013,
+  winery: 'Conti Torraiolo', varietal: 'Red Wine',
+  country: 'Italy')
+
+
+
+
