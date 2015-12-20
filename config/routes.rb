2ad040464b2570bff521315c12793fa1a01fb546
Mark Fordyce <mfordyce97@gmail.com>
2015-12-20 09:14:23 -0800

    Class 03 Homework

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,29 @@
 Rails.application.routes.draw do
 
-  get "/wines" => "wines#index", as: 'wines'
+  resources :wines
 
-  get 'wines/index'
+  # get "/wines" => "wines#index", as: 'wines'
+
+  # get "/wines/new" => "wines#new", as: 'new_wine'
+
+  # get 'wines/index'
 
 
 
-  get 'wines/show'
+  # get 'wines/show'
 
 
-  get "/wines/:id" => "wines#show", as: 'wine'
+  # get "/wines/:id" => "wines#show", as: 'wine'
+
+  # post "/wines" => "wines#create"
+
+
+  # get "/wines/:id/edit" => "wines#edit", as: 'edit_wine'
+  # patch "/wines/:id" => "wines#update"
+
+
+  # delete "/wines/:id" => "wines#destroy"
+
 
 
   # The priority is based upon order of creation: first created -> highest priority.
